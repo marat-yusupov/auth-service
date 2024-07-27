022d28ec53915ad3ab2236bcce71b29868b3cc42
m.yusupov <m.yusupov@omp.ru>
2024-07-27 10:38:36 +0300

    Verify by hash in db

--- app/database/auth_service_database.py
+++ app/database/auth_service_database.py
@@ -1,17 +1,39 @@
+from app.auth import verify_password
 from app.database.request_executor import RequestExecutor
 from app.models.auth_data import AuthData
-from utils.logger import log_error
+from utils.logger import log_error, log_warn
 
 
 class AuthServiceDatabase:
     def __init__(self):
         self.request_executor = RequestExecutor()
 
-    def get_auth_data(self, username: str, hashed_password: str) -> AuthData:
-        query = """
-SELECT account_id 
+    def get_auth_data(self, username: str, password: str) -> bool:
+        query = f"""
+SELECT password_hash
+FROM auth_data
+WHERE username = '{username}'
+"""
+        hashed_password_as_record = self.request_executor.execute_read_query(query)
+        if hashed_password_as_record is None:
+            return None
+        if len(hashed_password_as_record) == 0:
+            log_warn("User not found")
+            return None
+        if len(hashed_password_as_record) > 1:
+            log_error("Found more than one user!")
+            return None
+
+        hashed_password_tuple = hashed_password_as_record[0]
+        hashed_password = hashed_password_tuple[0]
+        if not verify_password(password, hashed_password):
+            log_warn("Verification failed")
+            return None
+
+        query = f"""
+SELECT account_id
 FROM auth_data 
-WHERE username='{username}' AND password_hash='{hashed_password}'
+WHERE username='{username}'
 """
         auth_data_as_record = self.request_executor.execute_read_query(query)
         if auth_data_as_record is None:
--- app/main.py
+++ app/main.py
@@ -9,11 +9,8 @@ security = HTTPBasic()
 
 @app.get("/authentification")
 def authentification(credentials: HTTPBasicCredentials = Depends(security)):
-    hashed_password = get_password_hash(credentials.password)
-    print(hashed_password)
-
     database = AuthServiceDatabase()
-    auth_data = database.get_auth_data(credentials.username, hashed_password)
+    auth_data = database.get_auth_data(credentials.username, credentials.password)
     if auth_data is None:
         return {"error": "User no found in database"}
     else:
